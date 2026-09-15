84e9ee47cc6bcfd0b9fbd4be166b139cd9a1b297
Sidh-mgl <sidhmangal@gmail.com>
2026-09-15 19:28:23 +0530

    Time: 6 ms (16.97%), Space: 8.8 MB (36.6%) - LeetHub

--- 0441-arranging-coins/0441-arranging-coins.cpp
+++ 0441-arranging-coins/0441-arranging-coins.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
-    int arrangeCoins(int n) {
-        int low = 1;
-        int high = n;
-        int ans;
+    int arrangeCoins(int n) {
+        int ans = 0;
+        int i = 1;
+        while(n >= i){
+            n -= i;
+            i++;
+        }
+        return i - 1;
 
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            long long csum = 1LL * mid * (mid + 1) / 2;
+    }
 
-            if (csum <= n) {
-                ans = mid;
-                low = mid + 1;
-            } else if (csum > n) {
-                high = mid - 1;
-            }
-        }
-
-        return ans;
-    }
 };
